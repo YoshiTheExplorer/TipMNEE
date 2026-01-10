5bbfd4f67495d401d274db8aef0fe190ae715660
Colin Yamada <colin.yamada@gmail.com>
2026-01-10 22:47:40 +0900

    init Java

---
+++ tipmnee/src/main/java/com/example/tipmnee/TipmneeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tipmnee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TipmneeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TipmneeApplication.class, args);
+	}
+
+}
